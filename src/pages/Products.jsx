712c2451e9a5eaeef7c55e1c47c3a319c496cde6
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-07-13 00:17:14 +0330

    added Card component and it's style, shortenText function in helpers to make titels short, and used react-icons

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,21 @@
+import Card from "../components/Card"
 import { useProducts } from "../context/ProductProvider"
+import styles from "./Products.module.css"
 
 function Products() {
+  const products = useProducts() //this is state from ProductProvider and on change ProductProvider will rerender then children components will rerender too
   console.log(useProducts())
-  return <div>Products</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <h1>Loading ...</h1>}
+        {products.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
+      </div>
+      <div>Sidebar</div>
+    </div>
+  )
 }
 
 export default Products
