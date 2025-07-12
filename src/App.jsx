3becc0eab8e8b8ea127b39563559e31fb3bdcfa4
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-07-12 16:07:47 +0330

    added routes of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+import PageNotFound from "./pages/PageNotFound"
+import Products from "./pages/Products"
+import ProductDetailsPage from "./pages/ProductDetailsPage"
+import CheckoutPage from "./pages/CheckoutPage"
+
 function App() {
-  return <div>app</div>
+  return (
+    <div>
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        
+      </Routes>
+    </div>
+  )
 }
 
 export default App
