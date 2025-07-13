3690e335933e36bab0a38210201a11d9a4f3c53d
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-07-13 21:18:16 +0330

    handeling, displaying search and category in the same time, added their functions in helpers

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,3 +1,19 @@
 const shortenText = (text) => text.split(" ").slice(0, 3).join(" ")
 
-export { shortenText }
+const searchProducts = (products, search) => {
+  if (!search) return products
+  const searchedProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search)
+  )
+  return searchedProducts
+}
+
+const categoriseProducts = (searchedProducts, category) => {
+  if (!category || category === "all") return searchedProducts
+  const categorisedProducts = searchedProducts.filter(
+    (searchedProduct) => searchedProduct.category === category
+  )
+  return categorisedProducts
+}
+
+export { shortenText, searchProducts, categoriseProducts }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,21 +1,81 @@
+import { useEffect, useState } from "react"
+
+import { ImSearch } from "react-icons/im"
+import { FaListUl } from "react-icons/fa"
+
 import Card from "../components/Card"
 import Loader from "../components/Loader"
 import { useProducts } from "../context/ProductProvider"
+import { categoriseProducts, searchProducts } from "../helpers/helper"
+
 import styles from "./Products.module.css"
 
 function Products() {
+  const [search, setSearch] = useState("")
+  const [query, setQuery] = useState({})
+  const [displayed, setDisplayed] = useState([])
+
   const products = useProducts() //this is state from ProductProvider and on change ProductProvider will rerender then children components will rerender too
-  console.log(useProducts())
+
+  useEffect(() => {
+    setDisplayed(products)
+  }, [products])
+
+  useEffect(() => {
+    const searchedProducts = searchProducts(products, query.search)
+    const categorisedProducts = categoriseProducts(
+      searchedProducts,
+      query.category
+    )
+    setDisplayed(categorisedProducts)
+  }, [query])
+
+  const searchHandler = () => {
+    setQuery((query) => ({ ...query, search }))
+  }
+
+  const categoryHandler = ({ tagName, innerText }) => {
+    if (tagName !== "LI") return
+    const category = innerText.toLowerCase()
+    setQuery((query) => ({ ...query, category }))
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.trim().toLowerCase())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>Sidebar</div>
-    </div>
+
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={(e) => categoryHandler(e.target)}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Mens's Clothing</li>
+            <li>Womens's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
