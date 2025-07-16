69e399c25789b1ca9ecf6f035737c656cdf5508d
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-07-16 17:29:02 +0330

    added style of PageNotFound component

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,19 @@
+import { TbError404 } from "react-icons/tb"
+
+import styles from "./PageNotFound.module.css"
+
+import { Link } from "react-router-dom"
+
 function PageNotFound() {
-  return <div>PageNotFound</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.notfound}>
+        <TbError404 />
+        <h1>Page Not Found !</h1>
+      </div>
+      <Link to="/products">Back to Home</Link>
+    </div>
+  )
 }
 
 export default PageNotFound
